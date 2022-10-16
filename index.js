f43cd0aac8337ff3ade4aa743d74e84e87f8acff
Lance Ellis <scdelance@gmail.com>
2022-10-15 21:20:08 -0500

    first-commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const Gun = require("gun");
+
+const app = express();
+const port = 8000;
+app.use(Gun.serve);
+
+const server = app.listen(port, () => {
+  console.log("Listening at: http://localhost://" + port);
+});
+
+var gun = Gun({ web: server });
+
+gun.get("incidents").put({
+  id: "1",
+  title: "test",
+  description: "test",
+  location: { latitude: 30.286489, longitude: -97.736718 },
+  image: "test",
+  date: "test",
+});
